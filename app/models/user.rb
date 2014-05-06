d6a2d6789bac4b20e773c12dc57f8e34a19a3686
Joel Mayer <joelmayer@Jon-Kytes-iMac.local>
2014-05-06 18:03:59 +0100

    fix avatars for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  after_create :get_gravatar
-  
+  # :lockable, :timeoutable and :omniauthable  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
@@ -24,21 +22,6 @@ class User < ActiveRecord::Base
   
   def auth
     Digest::MD5.hexdigest(self.name + self.encrypted_password)
-  end
-  
-  def gravatar_url
-    stripped_email = email.strip
-    downcase_email = stripped_email.downcase
-    hash = Digest::MD5.hexdigest(downcase_email)
-
-    "http://gravatar.com/avatar/#{hash}"
-  end
-  
-  def get_gravatar
-    if !self.avatar?
-       self.avatar = URI.parse(self.gravatar_url)
-       self.save
-    end
   end
   
   def avatar_url
