167c73da0f5f15f28866af7739adaf147ec995ef
Joel Mayer <joelmayer@Jon-Kytes-iMac.local>
2014-05-10 14:53:20 +0100

    Fix migrations

--- db/migrate/20140427163534_add_dragonfly_to_users.rb
+++ db/migrate/20140427163534_add_dragonfly_to_users.rb
@@ -1,9 +1,5 @@
 class AddDragonflyToUsers < ActiveRecord::Migration
   def change
-    remove_column :users, :avatar_file_name
-    remove_column :users, :avatar_content_type
-    remove_column :users, :avatar_file_size
-    remove_column :users, :avatar_updated_at
     add_column :users, :avatar_uid,  :string
     add_column :users, :avatar_name, :string
   end
