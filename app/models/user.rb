a61f9ae449ebfc03b168d0954c3211b4c35c5697
Joel Mayer <joelmayer@Jon-Kytes-iMac.local>
2014-05-15 19:02:56 +0100

    Add auto crop selection

--- app/models/user.rb
+++ app/models/user.rb
@@ -58,4 +58,16 @@ class User < ActiveRecord::Base
     md = self.avatar_cropping.match('(\d+):(\d+):(\d+)x(\d+)')
     { :x => md[1], :y => md[2],  :width  => md[3], :height => md[4] }
   end
+  
+  def crop_array
+    if self.avatar_cropping.nil?
+      [ 0, 0, 240, 240 ]
+    else
+      cropping = self.avatar_cropping.split(/[^\d]/)
+      array = cropping[2..3]
+      array << (cropping[0].to_i + cropping[2].to_i).to_s
+      array << (cropping[1].to_i + cropping[3].to_i).to_s
+      array
+    end
+  end
 end
