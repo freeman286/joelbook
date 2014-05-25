2a49e21a57ea4bbf7d40f07213373582424dcee1
Joel Mayer <joelmayer@Jon-Kytes-iMac.local>
2014-05-25 14:30:18 +0100

    Add avatars to search

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,8 @@ module ApplicationHelper
   def avatar_url(user)
     user.avatar.nil? ? user.gravatar_url : user.avatar.thumb(user.avatar_cropping).thumb("80x80").url
   end
+  
+  def search_avatar_url(user)
+    user.avatar.nil? ? user.gravatar_url + "?s=40"  : user.avatar.thumb(user.avatar_cropping).thumb("40x40").url
+  end
 end
