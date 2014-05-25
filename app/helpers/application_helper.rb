f3b39d188627d5e01600677ab1ae6ca7d17cd038
Joel Mayer <joelmayer@Jon-Kytes-iMac.local>
2014-05-25 14:49:19 +0100

    Make code dryer and add foundations for user profiles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,11 +10,7 @@ module ApplicationHelper
 		end
 	end
   
-  def avatar_url(user)
-    user.avatar.nil? ? user.gravatar_url : user.avatar.thumb(user.avatar_cropping).thumb("80x80").url
-  end
-  
-  def search_avatar_url(user)
-    user.avatar.nil? ? user.gravatar_url + "?s=40"  : user.avatar.thumb(user.avatar_cropping).thumb("40x40").url
+  def avatar_url(user, size="80")
+    user.avatar.nil? ? user.gravatar_url + "?s=#{size}" : user.avatar.thumb(user.avatar_cropping).thumb("#{size}x#{size}").url
   end
 end
