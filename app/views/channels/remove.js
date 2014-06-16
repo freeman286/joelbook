89b56bb6259d5b7c51bafb458fbd642c29d7c1bd
Joel Mayer <joelmayer@jon-kytes-imac.home>
2014-06-16 18:55:42 +0100

    Add smooth transitions for add and remove user from channels

--- app/views/channels/remove.js
+++ app/views/channels/remove.js
@@ -1,6 +1,7 @@
 $("#li-user-<%=@user.id%>").animate({height:'0px'}, 500)
+$(".ul-user").animate({height:'-=60px'}, 500);
 setTimeout(
   function() 
   {
-    $("#user-<%=@user.id%>").remove()
-  }, 5000);
+    $("li-user-#user-<%=@user.id%>").remove()
+  }, 1000);
