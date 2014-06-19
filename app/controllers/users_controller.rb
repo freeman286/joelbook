f72f88c90fc2ddfbec0714a819bebfdf7b38fec9
Joel Mayer <joelmayer@jon-kytes-imac.home>
2014-06-19 17:43:50 +0100

    Add backbone to user profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,5 +22,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @channel = Channel.find_private_channel(@user, current_user)
+    @posts = @user.posts
   end
 end
