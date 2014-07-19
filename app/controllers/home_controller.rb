ba8805edb90db1a74fe4caee7787a371e2149dce
Joel Mayer <joelmayer@jon-kytes-imac.home>
2014-07-19 12:42:30 +0100

    Improve homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @posts = Post.all.select{|p| p.channel.public?}
-    @events = Event.all.pop(5)
+    @events = Event.all.pop(2)
   end
 end
