cf2423d16c6c5a3fbca550906213be4510c9a61b
Joel Mayer <molemayer@gmail.com>
2014-07-21 17:09:38 +0100

    Change devise forms so privacy can be altered

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -8,10 +8,12 @@ module DeviseHelper
       resource: resource.class.model_name.human.downcase)
 
     html = <<-HTML
-    <div class="alert alert-danger alert-block">
-      <button type="button" class="close" data-dismiss="alert">x</button>
-      <h4>#{sentence}</h4>
-      #{messages}
+    <div class="col-md-12">
+      <div class="alert alert-danger alert-block">
+        <button type="button" class="close" data-dismiss="alert">x</button>
+        <h4>#{sentence}</h4>
+        #{messages}
+      </div>
     </div>
     HTML
 
