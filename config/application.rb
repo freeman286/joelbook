c9cf3086884a277ca3753265e73dd57091f1d2be
Joel Mayer <joelmayer@jon-kytes-imac.home>
2014-08-09 13:27:01 +0100

    Heroku assets fix

--- config/application.rb
+++ config/application.rb
@@ -60,6 +60,8 @@ module RailsRealtime
     config.assets.version = '1.0'
     
     config.autoload_paths += %W(#{config.root}/lib)
+    
+    config.assets.initialize_on_precompile = false
 
   end
 end
