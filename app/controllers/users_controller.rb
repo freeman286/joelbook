ad80a6527005fe9de58c8653532d0a8673740032
Joel Mayer <joelmayer@jon-kytes-imac.home>
2014-08-09 09:49:26 +0100

    Fix the activity stream on the profile of a user not working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @channel = Channel.find_private_channel(@user, current_user)
-    @posts = @user.posts.select{|p| p.channel.public?}
+    @posts = @user.posts.select{|p| p.channel.public?}.last(5)
   end
   
   def images
