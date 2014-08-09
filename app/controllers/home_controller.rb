1463ee4f752769597859bd356755ecfa6792f9c1
Joel Mayer <joelmayer@jon-kytes-imac.home>
2014-08-09 10:10:20 +0100

    Bug fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.all.select{|p| p.channel.public?}
+    @posts = Post.all.select{|p| p.channel.public?}.last(10)
     @events = Event.all.pop(2)
   end
 end
