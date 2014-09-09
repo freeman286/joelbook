e3d013e60591d89aa2d21abc77e8d19b9723e583
freeman286 <molemayer@gmail.com>
2014-09-09 17:49:42 +0100

    Bug fixes

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,10 +1,10 @@
 class Channel < ActiveRecord::Base
   has_and_belongs_to_many :users
-  
+
   belongs_to :owner_user, class_name: 'User', foreign_key: 'owner_user_id'
   belongs_to :secondary_owner_user, class_name: 'User', foreign_key: 'secondary_owner_user_id'
-  
-  has_many :posts
+
+  has_many :posts, dependent: :destroy
 
   attr_accessible :owner_user_id, :secondary_owner_user_id, :name, :private, :public, :page
 
@@ -14,19 +14,19 @@ class Channel < ActiveRecord::Base
   validates :secondary_owner_user_id, presence: true, if: :private?
 
   validates :name, presence: true
-  
+
   def includes_user?(user)
     self.users.include?(user)
   end
-  
+
   def current_user_valid
     User.current && self.owner_user == User.current
   end
-  
+
   def self.find_private_channel(user_1, user_2)
     (Channel.where(:owner_user_id => user_1.id, :secondary_owner_user_id => user_2.id, :private => true) + Channel.where(:owner_user_id => user_2.id, :secondary_owner_user_id => user_1.id, :private => true)).first
   end
-  
+
   def self.search(words)
     channels = Set.new
 
@@ -41,4 +41,3 @@ class Channel < ActiveRecord::Base
     end
   end
 end
-
