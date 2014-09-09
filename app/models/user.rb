f05d8f9ef805e220e6bb9f8b1b1005c5f747d198
freeman286 <molemayer@gmail.com>
2014-09-09 18:20:34 +0100

    Validation update

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,129 +4,133 @@ class User < ActiveRecord::Base
   before_create :set_user_friendship_times
     # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable  
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email,
-                  :password, 
-                  :password_confirmation, 
-                  :current_password, 
-                  :remember_me, 
-                  :name, 
-                  :avatar, 
-                  :avatar_cropping, 
-                  :avatar_width, 
-                  :avatar_height, 
-                  :page, 
-                  :last_destroyed_user_friendship_at, 
+                  :password,
+                  :password_confirmation,
+                  :current_password,
+                  :remember_me,
+                  :name,
+                  :avatar,
+                  :avatar_cropping,
+                  :avatar_width,
+                  :avatar_height,
+                  :page,
+                  :last_destroyed_user_friendship_at,
                   :last_destroyed_accepted_user_friendship_at,
                   :last_destroyed_pending_user_friendship_at,
                   :last_destroyed_blocked_user_friendship_at,
                   :last_destroyed_ignored_user_friendship_at,
                   :images_visable,
-                  :email_visable, 
+                  :email_visable,
                   :friends_visable,
                   :can_be_messaged,
                   :images_visable_to_friends,
-                  :email_visable_to_friends, 
+                  :email_visable_to_friends,
                   :friends_visable_to_friends,
                   :can_be_messaged_by_friends
-                  
+
   # attr_accessible :title, :body
   attr_accessor :current_password
 
   has_many :posts
-  
+
   has_and_belongs_to_many :channels
-  
+
   has_many :user_friendships, dependent: :destroy
 
   has_many :friends, through: :user_friendships,
                      conditions: { user_friendships: {state: 'accepted' }},
                      dependent: :destroy
-                   
+
   has_many :pending_user_friendships, class_name: 'UserFriendship',
                                       foreign_key: :user_id,
                                       conditions: { state: 'pending'},
                                          dependent: :destroy
-                                    
-                                    
+
+
   has_many :pending_friends, through: :pending_user_friendships, source: :friend, dependent: :destroy
 
   has_many :requested_user_friendships, class_name: 'UserFriendship',
                                       foreign_key: :user_id,
                                       conditions: { state: 'requested'}, dependent: :destroy
-                                    
+
   has_many :requested_friends, through: :requested_user_friendships, source: :friend, dependent: :destroy
 
   has_many :blocked_user_friendships, class_name: 'UserFriendship',
                                       foreign_key: :user_id,
                                       conditions: { state: 'blocked'},
                                       dependent: :destroy
-                                    
+
   has_many :blocked_friends, through: :blocked_user_friendships, source: :friend, dependent: :destroy
 
   has_many :accepted_user_friendships, class_name: 'UserFriendship',
                                       foreign_key: :user_id,
-                                      conditions: { state: 'accepted'},    
+                                      conditions: { state: 'accepted'},
                                       dependent: :destroy
-                                    
+
   has_many :accepted_friends, through: :accepted_user_friendships, source: :friend, dependent: :destroy
-  
+
   has_many :declined_user_friendships, class_name: 'UserFriendship',
                                       foreign_key: :user_id,
-                                      conditions: { state: 'declined'},    
+                                      conditions: { state: 'declined'},
                                       dependent: :destroy
-                                    
+
   has_many :declined_friends, through: :declined_user_friendships, source: :friend, dependent: :destroy
-  
+
   has_many :ignored_user_friendships, class_name: 'UserFriendship',
                                       foreign_key: :user_id,
-                                      conditions: { state: 'ignored'},    
+                                      conditions: { state: 'ignored'},
                                       dependent: :destroy
-                                    
+
   has_many :ignored_friends, through: :ignored_user_friendships, source: :friend, dependent: :destroy
-  
+
   dragonfly_accessor :avatar
   validates_property :format, of: :avatar, in: ['jpeg', 'png', 'gif']
-  
+
   has_many :images
-    
-  validates_presence_of :name
-  validates_uniqueness_of :name
-  
+
+  validates :name, presence: true,
+            uniqueness: true,
+            format: {
+            with: /^[a-zA-Z0-9_-]+$/,
+            message: 'Must contain no spaces or foreign characters.'
+            }
+
   def auth
     Digest::MD5.hexdigest(self.name + self.encrypted_password)
   end
-  
-  def avatar_url  
+
+  def avatar_url
     self.avatar.nil? ? self.gravatar_url : self.avatar.thumb(self.avatar_cropping).thumb("80x80").url
   end
-  
+
   def self.current
     Thread.current[:user]
   end
-  
+
   def self.current=(user)
     Thread.current[:user] = user
   end
-  
+
   def update_posts
     Post.where(:user_name => self.name).each do |post|
       post.user_img_url = self.avatar_url
       post.save
     end
   end
-  
+
   def update_channels
     self.channels.each do |channel|
       channel.name = channel.name.gsub("#{self.name_was} ", "#{self.name} ")
       channel.save
     end
   end
-  
+
   def gravatar_url
     stripped_email = email.strip
     downcase_email = stripped_email.downcase
@@ -134,13 +138,13 @@ class User < ActiveRecord::Base
 
     "http://gravatar.com/avatar/#{hash}"
   end
-  
+
   def crop_hash
     self.avatar_cropping
     md = self.avatar_cropping.match('(\d+):(\d+):(\d+)x(\d+)')
     { :x => md[1], :y => md[2],  :width  => md[3], :height => md[4] }
   end
-  
+
   def self.search(words, exceptions = [])
     users = Set.new
 
@@ -152,13 +156,13 @@ class User < ActiveRecord::Base
       users.first - exceptions
     else
       nil
-    end   
+    end
   end
-  
+
   def notifications
     Notification.where(:owner_user_id => self.id)
   end
-  
+
   def set_user_friendship_times
     self.last_destroyed_user_friendship_at =
     self.last_destroyed_accepted_user_friendship_at =
