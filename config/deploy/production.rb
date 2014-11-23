5cf580df2f06e9362885d90f403e87a1422661e5
Joel Mayer <molemayer@gmail.com>
2014-11-23 16:25:44 +0000

    Change role

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '178.62.28.141', user: 'deploy', roles: %w{web app db}
+server '178.62.28.141', user: 'joelmayer', roles: %w{web app db}
