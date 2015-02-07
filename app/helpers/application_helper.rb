b35cec301e8429718037913e110e72c7834d1abf
Joel Mayer <molemayer@gmail.com>
2015-02-07 13:26:54 +0000

    Fix image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,11 +9,15 @@ module ApplicationHelper
 			""
 		end
 	end
-  
+
   def avatar_url(user, size="80")
-    user.avatar.nil? ? user.gravatar_url + "?s=#{size}" : user.avatar.thumb(user.avatar_cropping).thumb("#{size}x#{size}").url
+    if user.present?
+			user.avatar.nil? ? user.gravatar_url + "?s=#{size}" : user.avatar.thumb(user.avatar_cropping).thumb("#{size}x#{size}").url
+		else
+			"http://placehold.it/#{size}x#{size}"
+		end
   end
-  
+
   def update_action_verbiage(friendship)
   case friendship.state
     when 'pending'
@@ -21,19 +25,19 @@ module ApplicationHelper
     when 'requested'
       'Accept'
     when 'declined'
-      'Accept'  
+      'Accept'
     when 'accepted'
       'Unfriend'
     when 'blocked'
       'Unblock User'
     end
   end
-  
+
   def link_to_notification_resource(notification)
-    
+
   end
-  
+
   def image_for_notification(notification, size, options)
     image_tag(avatar_url(notification.secondary_owner_user, size="25"), options)
-  end 
+  end
 end
